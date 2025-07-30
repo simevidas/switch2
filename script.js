aa0208aff949701f245457f814d0633e253f6479
Šime Vidas <sime.vidas@gmail.com>
2025-07-30 08:07:33 +0200

    Revert setting --u on page load
    This breaks things on mobile. Setting --u can only be a desktop solution.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 {
-  document.documentElement.style = `--u: ${window.innerHeight / 100}px`;
+  // document.documentElement.style = `--u: ${window.innerHeight / 100}px`;
 
   // category: 1 exclusive, .1 retail, .2 digital-only; 2 “edition”, .1 “plus”, .2 plain
   // developer: 1 Nintendo, 2 subsidiary, 3 close partner, 4 other developer
