7ec7c60c5e6e80c2d6fba3e348896fdbbc1923e9
Šime Vidas <sime.vidas@gmail.com>
2025-08-05 00:18:17 +0200

    move build instructions to script

--- INSTRUCTIONS.md
+++
@@ -1,6 +0,0 @@
-## Building and deploying
-
-1. update version number in local.html
-2. node build.mjs
-3. commit both local.html and newly generated index.html with message “version X.Y.Z”
-4. git push
--- build.mjs
+++ build.mjs
@@ -12,3 +12,10 @@ html = html
   .replace('<script src="script.js"></script>', `<script>${js}</script>`);
 
 fs.writeFileSync('./index.html', html);
+
+// Building and deploying:
+
+// 1. update version number in local.html
+// 2. node build.mjs
+// 3. commit both local.html and newly generated index.html with message “version X.Y.Z”
+// 4. git push
